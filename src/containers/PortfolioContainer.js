f8f15e08436978f0c6f459333e1d1d9f623534ee
J-Agens <johntagens@gmail.com>
2019-05-20 17:31:54 -0400

    Done.

--- src/containers/PortfolioContainer.js
+++ src/containers/PortfolioContainer.js
@@ -3,12 +3,22 @@ import Stock from '../components/Stock'
 
 class PortfolioContainer extends Component {
 
+  handleClick = (stock) => {
+    this.props.sellStock(stock);
+  }
+
+  generateStockCards = () => {
+    return this.props.stocks.map(stock => {
+      return <Stock key={stock.id} stock={stock} handleClick={() => {this.handleClick(stock)}}/>
+    });
+  }
+
   render() {
     return (
       <div>
         <h2>My Portfolio</h2>
           {
-            //render your portfolio stocks here
+            this.generateStockCards()
           }
       </div>
     );
