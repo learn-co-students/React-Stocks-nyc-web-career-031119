cc82561e04a3228f45e56c69db8fda7fb8f92446
jyan95 <jyan0602@gmail.com>
2019-05-21 17:54:50 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,76 @@ import React, { Component } from 'react';
 import Header from './components/Header'
 import MainContainer from './containers/MainContainer'
 
+const API = 'http://localhost:3000/stocks';
+
 class App extends Component {
+  state = {
+    all: [],
+    stocks: []
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(res => res.json())
+    .then(data => {
+      let newStocks = data.map(s => ({...s, owned:false}));
+      // console.log(newStocks);
+      this.setState({all:newStocks})
+      this.setState({stocks:this.state.all});
+    })
+  }
+
+  filterByType = (e) => {
+    let filtered = this.state.all.filter(s => s.type === e.target.value);
+    this.setState({stocks:filtered});
+  }
+
+  buySell = (id) => {
+    // console.log('ya done clicked', id);
+    let updatedStocks = this.state.stocks.map(
+      s => {
+      if (s.id === id) {
+          return {...s, owned: !s.owned}
+        } else {
+          return s
+        }
+      }
+    );
+    this.setState({stocks:updatedStocks});
+  }
+
+  sortAlphabetically = (e) => {
+    if(e.target.value){
+      let alphabetical = this.state.stocks.sort((a,b) => a.name > b.name ? 1 : -1);
+      this.setState({stocks: alphabetical});
+    } else {
+      this.setState({stocks:this.state.all})
+    }
+    console.log('sorting alphabetically');
+
+  }
+
+  sortByPrice = (e) => {
+    if(e.target.value){
+      let byPrice = this.state.stocks.sort((a,b) => a.price - b.price);
+      this.setState({stocks: byPrice});
+    } else {
+      this.setState({stocks:this.state.all})
+    }
+    console.log('sorting by price');
+  }
+
   render() {
     return (
       <div>
-        <Header/>
-        <MainContainer/>
+        <Header />
+        <MainContainer
+          stocks={this.state.stocks}
+          buySell={this.buySell}
+          filterByType={this.filterByType}
+          alphaSort={this.sortAlphabetically}
+          priceSort={this.sortByPrice}
+        />
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,28 @@
 import React from 'react';
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
     <div>
 
       <strong>Sort by:</strong>
       <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
+        <input type="checkbox" value="Alphabetically"  onChange={props.alphaSort}/>
         Alphabetically
       </label>
       <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
+        <input type="checkbox" value="Price" onChange={props.priceSort}/>
         Price
       </label>
       <br/>
 
       <label>
         <strong>Filter:</strong>
-        <select onChange={null}>
+        <select onChange={props.filterByType}>
           <option value="Tech">Tech</option>
           <option value="Sportswear">Sportswear</option>
           <option value="Finance">Finance</option>
         </select>
       </label>
-
-
     </div>
   );
 }
