3ac322d04ed83885d4aa549eccfd415aff46344a
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-05-21 09:48:36 -0400

    Done.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,50 @@
 import React from 'react';
 
-const SearchBar = () => {
-  return (
-    <div>
+class SearchBar extends React.Component {
+  state = {
+    alpha: false,
+    price: false
+  }
 
-      <strong>Sort by:</strong>
-      <label>
-        <input type="radio" value="Alphabetically" checked={null} onChange={null}/>
-        Alphabetically
-      </label>
-      <label>
-        <input type="radio" value="Price" checked={null} onChange={null}/>
-        Price
-      </label>
-      <br/>
+  toggleSort = (e) => {
+    const sorted = e.target.value
+    if (e.target.value === "Alphabetically") {
+      this.setState( {alpha: true, price: false}, () => this.props.toggleSort(sorted))
+    } else if (e.target.value === "Price") {
+      this.setState({alpha: false, price: true}, () => this.props.toggleSort(sorted))
+    }
+  }
 
-      <label>
-        <strong>Filter:</strong>
-        <select onChange={null}>
-          <option value="Tech">Tech</option>
-          <option value="Sportswear">Sportswear</option>
-          <option value="Finance">Finance</option>
-        </select>
-      </label>
+  render () {
+
+    return (
+      <div>
+
+        <strong>Sort by:</strong>
+        <label>
+          <input type="radio" value="Alphabetically" checked={this.state.alpha} onChange={this.toggleSort}/>
+          Alphabetically
+        </label>
+        <label>
+          <input type="radio" value="Price" checked={this.state.price} onChange={this.toggleSort}/>
+          Price
+        </label>
+        <br/>
+
+        <label>
+          <strong>Filter:</strong>
+          <select onChange={this.props.handleDropDown}>
+            <option value="All">All</option>
+            <option value="Tech">Tech</option>
+            <option value="Sportswear">Sportswear</option>
+            <option value="Finance">Finance</option>
+          </select>
+        </label>
 
 
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 
--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,20 +5,75 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state= {
+    filteredStocks: [],
+    portfolio: [],
+    allStocks: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/stocks')
+    .then(r => r.json())
+    .then(data => this.setState({allStocks: data, filteredStocks: data}))
+  }
+
+  handlePurchase = (purchasedStock) => {
+    const fil = this.state.filteredStocks.filter(stock => {
+      return stock.id !== purchasedStock.id
+    })
+    this.setState({filteredStocks: fil, portfolio: [...this.state.portfolio, purchasedStock]}, () => console.log(this.state))
+  }
+
+  handleSale = (soldStock) => {
+    const fil = this.state.portfolio.filter(stock => {
+      return stock.id !== soldStock.id
+    })
+    this.setState({portfolio: fil, filteredStocks: [...this.state.filteredStocks, soldStock]}, () => console.log(this.state))
+  }
+
+  handleDropDown = (e) => {
+    if (e === undefined || e.target.value === "All"){
+      this.setState({filteredStocks: this.state.allStocks})
+    } else {
+      const filterStocks = this.state.allStocks.filter(stock => {
+        return stock.type === e.target.value
+      })
+
+      this.setState({filteredStocks: filterStocks})
+    }
+  }
+
+  toggleSort = (sortedValue) => {
+    console.log(sortedValue)
+    if (sortedValue === "Alphabetically"){
+      const sortedAlphaStocks = this.state.filteredStocks.sort((a, b) => {
+        return a.name > b.name ? 1 : -1
+      })
+      this.setState({filteredStocks: sortedAlphaStocks})
+    } else if (sortedValue === "Price"){
+      const sortedPriceStocks = this.state.filteredStocks.sort((a, b) => {
+
+        return b.price - a.price
+      })
+      console.log(sortedPriceStocks)
+      this.setState({filteredStocks: sortedPriceStocks})
+    }
+  }
+
   render() {
     return (
       <div>
-        <SearchBar/>
+        <SearchBar handleDropDown={this.handleDropDown} toggleSort={this.toggleSort}/>
 
           <div className="row">
             <div className="col-8">
 
-              <StockContainer/>
+              <StockContainer stocks={this.state.filteredStocks} handlePurchase={this.handlePurchase}/>
 
             </div>
             <div className="col-4">
 
-              <PortfolioContainer/>
+              <PortfolioContainer stocks={this.state.portfolio} handleSale={this.handleSale}/>
 
             </div>
           </div>
